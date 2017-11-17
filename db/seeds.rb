66768c46b27d313cbc3ef650bf4578fc1f1805d1
Rich Ogrady <rjogrady21@gmail.com>
2017-11-17 19:20:35 +0100

    New cocktails made in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,51 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'json'
-require 'open-uri'
+# #   Character.create(name: 'Luke', movie: movies.first)
+# require 'json'
+# require 'open-uri'
 
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-ingredients_JSON = open(url).read
-drinks = JSON.parse(ingredients_JSON)
-ingredients = drinks["drinks"]
-ingredients.each do |ingredient|
-  Ingredient.create!(
-    name: ingredient["strIngredient1"]
-  )
+# url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# ingredients_JSON = open(url).read
+# drinks = JSON.parse(ingredients_JSON)
+# ingredients = drinks["drinks"]
+# ingredients.each do |ingredient|
+#   Ingredient.create!(
+#     name: ingredient["strIngredient1"]
+#   )
+# end
+puts "Delete all seed data"
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+
+require 'rest-client'
+require 'json'
+
+cocktail_rows = 100
+ingredients_url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+cocktail_random_url = "http://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+def get_json(url)
+ JSON.parse(RestClient.get(url))
+end
+
+puts "Creating rows for Ingredients"
+get_json(ingredients_url)["drinks"].each do |ingredient|
+ Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+puts "Adding #{cocktail_rows} rows to Cocktails"
+cocktail_rows.times do
+ cocktail_hash = get_json(cocktail_random_url)["drinks"][0]
+ cocktail_obj = Cocktail.create(name: cocktail_hash["strDrink"], remote_photo_url: cocktail_hash["strDrinkThumb"])
+
+ 15.times do |n|
+   index_number = n + 1
+   ingredient_name = cocktail_hash["strIngredient#{index_number}"]
+   unless ingredient_name.nil? || ingredient_name.empty?
+     ingredient = Ingredient.where(name: ingredient_name).first
+   end
+ end
 end
